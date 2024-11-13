1d5dc9f486577099d35b2c9434b5d75606f5df66
88phantom88 <154367140+88phantom88@users.noreply.github.com>
2024-11-13 19:43:34 +0300

    Update my.py

--- my.py
+++ my.py
@@ -1,1 +1,13 @@
 print("hello world!")
+import random
+
+simvols = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+lenght = int(input("введите длинну пароля"))
+
+password = ""
+
+for i in range(lenght):
+    password += random.choice(simvols)
+
+print(password)
