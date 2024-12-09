7ee56443d79c0de0f283650ed37e45728c504edd
88phantom88 <154367140+88phantom88@users.noreply.github.com>
2024-12-09 20:55:59 +0300

    Update knobaBot.py
    Добавил новые функции, абсолютно между собой не связанные, но прикольные

--- knobaBot.py
+++ knobaBot.py
@@ -18,6 +18,33 @@ def send_heh(message):
     count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
     bot.reply_to(message, "he" * count_heh)
 
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    with open('images/kak_pervie_programmisti_programmirovali.jpeg', 'rb') as f:
+        bot.send_photo(message.chat.id, f)
+
+@bot.message_handler(commands=['mem2'])
+def send_mem(message):
+    with open('images/9e58e8a14f56f11ad66fa46b6d81407e.jpg', 'rb') as f:
+        bot.send_photo(message.chat.id, f)
+
+def get_duck_image_url():
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.message_handler(commands=['duck'])
+def duck(message):
+    '''По команде duck вызывает функцию get_duck_image_url и отправляет URL изображения утки'''
+    image_url = get_duck_image_url()
+    bot.reply_to(message, image_url)
+
+@bot.message_handler(commands=['commands'])
+def send_name(message):
+    bot.reply_to(message, 'hello, name, heh(число), mem, mem2, duck')
+
+
 
 # Запуск бота
 bot.polling()
